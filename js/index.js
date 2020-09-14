62862df35d9c30c4e1232de15c2614a2bc7f64ff
JinOptimist <88jin@mail.ru>
2020-09-14 20:13:18 +0300

    example

--- js/index.js
+++ js/index.js
@@ -1,24 +1,33 @@
 $(document).ready(function(){
+	
 	$('.login-popup').hide();
 	$('.login-popup .close').click(function(){
 		$('.login-popup').hide();
 	});
 	
-	$('.process').click(function(){
-		var data = $('.data').val() - 0;
+	$('.coin .process').click(function(){
+		var data = $('.coin .data').val() - 0;
+		
+		var coinText = calcCoinText(data);
+		
+		$('.coin .result').text(coinText);
+	});
+	
+	function calcCoinText(number){
+		return 'двенадцать';
+	}
+	
+	$('.encrypt .process').click(function(){
+		var data = $('.encrypt .data').val() - 0;
 		
 		var encryptData = encryptNumber(data);
 		
-		$('.result').text(encryptData);
+		$('.encrypt .result').text(encryptData);
 	});
 	
 	function encryptNumber(data){
-		var encryptData;
-		switch(data){
-			case 0:
-				encryptData = 4;
-				break;	
-		}
+		var numbersText = ['ноль', 'один', 'два', 'три'];
+		var encryptData = numbersText[data].length;
 		return encryptData;
 	}
 });
