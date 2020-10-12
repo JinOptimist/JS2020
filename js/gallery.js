0736c7ec2b16edda4f21852be2461c67b0ba0c60
JinOptimist <88jin@mail.ru>
2020-10-12 19:15:41 +0300

    Add array for gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+	
+	var goods = [
+		{
+			name: 'Blond',
+			url: 'http...'
+		},
+		{
+			name: 'Red',
+			url: 'http..2'
+		},
+	];
+	
 	$('.login-popup').hide();
 	
 	$('.login-popup .close').click(function(){
@@ -6,77 +18,44 @@ $(document).ready(function(){
 	});
 	
 	$('.goods.first .add').click(function(){
-		var name = $('.goods.first .new-image-name').val();
-		var url = $('.goods.first .new-image-url').val();
+		var newName = $('.goods.first .new-image-name').val();
+		var newUrl = $('.goods.first .new-image-url').val();
 		
-		var goodsDiv = $('<div>');
-		goodsDiv.addClass('goods');
-		
-		var divName = $('<div>');
-		divName.text(name);
-		
-		var divImg = $('<div>');
-		divImg.addClass('image');
-		var img = $('<img>');
-		img.attr('src', url);
-		divImg.append(img);
-		
-		goodsDiv.append(divName);
-		goodsDiv.append(divImg);
-		
-		$('.content').append(goodsDiv);
+		var good = {
+			name: newName,
+			url: newUrl
+		};
+		goods.push(good);
+		drawGoods();
 	});
+	
+	function drawGoods(){
+		$('.content .goods:not(.first)').remove();
+		
+		//sort goods
+		
+		for	(var i = 0; i< goods.length; i++){
+			var good = goods[i];
+			var goodsDiv = $('<div>');
+		
+			goodsDiv.addClass('goods');
+			
+			var divName = $('<div>');
+			divName.text(good.name);
+			
+			var divImg = $('<div>');
+			divImg.addClass('image');
+			var img = $('<img>');
+			img.attr('src', good.url);
+			divImg.append(img);
+			
+			goodsDiv.append(divName);
+			goodsDiv.append(divImg);
+			
+			$('.content').append(goodsDiv);
+		}
+		
+		
+	}
 });
-
-
-// var condition0 = true;
-// var condition1 = condition0 && false;
-
-// var condition2 = condition0 || condition1;
-
-// var condition3 = condition2 || (condition1 && condition0);
-
-// var condition3 = !(condition1 && condition0);
-
-
-// var num1 = 2;
-// var num2 = 15;
-
-// //7
-// var answer = sum(num1, num2);
-
-// //75
-// var answer2 = sum(60, num2);
-
-// var answer3 = sum(50, 100);
-
-// //167 - 1050
-// var answer4 = sum(answer, answer3) - sum(5, 7);
-
-// var answer4 = sum(sum(60, 80), sum(1, 1)) - sum(5, 7);
-
-// function sum(anyNumber1, anyNumber2){
-	// return anyNumber1 + anyNumber2;
-// }
-
-// for(var i = 0; i < someArray.length ; i++){
-	// someArray[i];
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
